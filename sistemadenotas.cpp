3877e1f5162e029d0f4220cb0d9f02f3a808017c
Kauan Farias <kauandefarias@yahoo.com.br>
2025-05-20 09:36:41 -0300

    ✨ feat: pedindo nome e RGM e acentuação em pt
    Para fins de melhor identificação sobre quem possui as notas informadas, também é pedido o nome e RGM desse aluno. Além disso, a biblioteca <locale.h> foi adicionada para que as acentuações em português não sejam exibidas como caracteres errados no terminal.

--- sistemadenotas.cpp
+++ sistemadenotas.cpp
@@ -1,23 +1,34 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
 	float a1, a2, af, soma;
+	char nomeAluno[20];
+	int RGM[8];
+	
+	printf("Digite o nome do aluno: ");
+	scanf("%s", nomeAluno);
+	
+	printf("Digite o RGM do aluno: ");
+	scanf("%d", RGM);
 	
 	do {
-		printf("Digite a nota da A1: ");
+		printf("- Digite a nota da A1: ");
 		scanf("%f", &a1);
 	} while (a1 < 0 || a1 > 5);
 	
 	do {
-		printf("Digite a nota da A2: ");
+		printf("- Digite a nota da A2: ");
 		scanf("%f", &a2);
 	} while (a2 < 0 || a2 > 5);
 
 	soma = a1 + a2;
 	if (soma >= 6) {
-		printf("\nO aluno foi aprovado com média %.2f.", soma);
+		printf("\n%s foi aprovado com média %.2f.", nomeAluno, soma);
 	} else {
-		printf("\nO aluno foi reprovado com média %.2f.", soma);
+		printf("\n%s foi reprovado com média %.2f.", nomeAluno, soma);
 	}
 
 	return 0;
