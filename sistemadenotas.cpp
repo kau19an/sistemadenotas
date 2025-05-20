fab1a33b3e468f61700a48cdc17a22678812c5a8
Kauan Farias <kauandefarias@yahoo.com.br>
2025-05-20 09:23:40 -0300

    ✨ feat: aceitando notas apenas entre 0-5
    Agora, com a estrutura 'do..while', o programa não aceitará notas abaixo de 0 ou acima de 5, de modo que a nota máxima seja sempre 10 e a mínima, 0.

--- sistemadenotas.cpp
+++ sistemadenotas.cpp
@@ -2,16 +2,22 @@
 
 int main() {
 	float a1, a2, af, soma;
-	printf("Digite a nota da A1: ");
-	scanf("%f", &a1);
-	printf("Digite a nota da A2: ");
-	scanf("%f", &a2);
 	
+	do {
+		printf("Digite a nota da A1: ");
+		scanf("%f", &a1);
+	} while (a1 < 0 || a1 > 5);
+	
+	do {
+		printf("Digite a nota da A2: ");
+		scanf("%f", &a2);
+	} while (a2 < 0 || a2 > 5);
+
 	soma = a1 + a2;
 	if (soma >= 6) {
-		printf("Aprovado.");
+		printf("\nO aluno foi aprovado com média %.2f.", soma);
 	} else {
-		printf("Reprovado.");
+		printf("\nO aluno foi reprovado com média %.2f.", soma);
 	}
 
 	return 0;
