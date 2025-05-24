0778a84801f88dceb3dbd7ec5e31a65a986a4137
Kauan Farias <kauandefarias@yahoo.com.br>
2025-05-24 17:56:50 -0300

    ✨ feat: readaptando o ex. da prof. p/ projeto, adicionando opção 5 e verificações

--- sistemadenotas.cpp
+++ sistemadenotas.cpp
@@ -1,110 +1,192 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
+#include <stdlib.h> // Para 'exit()'
+#include <string.h> // Para 'strcpy()'
+#include <locale.h> // Para 'setlocale()'
 
 struct Aluno {
-	char nome[20];
-	int RGM[8];
-	float a1, a2, af; 
-};
-
-struct Login {
-	char usuario[10];
-	int senha;
+	char nome[50];
+	int RGM;
+	float a1, a2, af, media;
+    char situacao[10]; // Para "Aprovado" ou "Reprovado"
 };
 
 // Funções do programa
 int abrirMenu() {
     int escolha;
 
-    printf("/---------- MENU ---------\\\n");
-    printf("|  (1) Criar usuário      |\n");
-    printf("|  (2) Listar usuários    |\n");
-    printf("|  (3) Atualizar usuário  |\n");
-    printf("|  (4) Deletar usuário    |\n");
-    printf("|  (0) Sair               |\n");
-    printf("\\-------------------------/\n");
-    printf("   Escolha uma opção: ");
+    printf("------- MENU -------\n");
+    printf("(1) Cadastrar aluno\n"); // C
+    printf("(2) Listar alunos\n");   // R
+    printf("(3) Atualizar aluno\n"); // U
+    printf("(4) Deletar aluno\n");   // D
+    printf("(5) Atribuir notas\n");
+    printf("(6) Corrigir notas\n");
+    printf("(7) Deletar notas\n");
+    printf("(0) Sair\n\n");
+    printf("> Escolha uma opção: ");
     scanf("%d", &escolha);
 
     return escolha;
 }
 
 // Opção 1 do menu
-void criarUsuarios(struct Login usuarios[], int *total) {
-    usuarios[*total];
+void cadastrarAluno(struct Aluno aluno[], int *total) {
+    aluno[*total];
     
-    printf("\n- Digite o nome do usuário (máx. 9 letras): ");
-    scanf("%9s", usuarios[*total].usuario);
-    printf("- Digite a senha (apenas números): ");
-    scanf("%d", &usuarios[*total].senha);
+    printf("\n> Digite o nome completo do aluno (sem espaços): ");
+    scanf("%s", aluno[*total].nome);
+    printf("> Digite o RGM: ");
+    scanf("%d", &aluno[*total].RGM);
     printf("\nVocê cadastrou:");
-    printf("\n- Nome de usuário: %s", usuarios[*total].usuario);
-    printf("\n- Senha: %d\n\n", usuarios[*total].senha);
+    printf("\n- %s (RGM: %d)\n\n", aluno[*total].nome, aluno[*total].RGM);
     (*total)++;
 }
 
 // Opção 2 do menu
-void listarUsuarios(struct Login usuarios[], int total) {    
+void listarAlunos(struct Aluno aluno[], int total) {    
     // Caso o usuário deseja ver a lista mas não há nenhum cadastrado
     if (total == 0) {
-        printf("\nNenhum usuário foi cadastrado até o momento.\n\n");
+        printf("\n(!) Nenhum aluno foi cadastrado até o momento.\n\n");
         return; // Para não continuar com as linhas abaixo
     }
 
-    printf("\nLista de usuários cadastrados:\n");
+    printf("\nLista de alunos cadastrados:\n");
     for (int i = 0; i < total; i++) {
-        printf("(%d) Usuário: %s - Senha: %d\n", i, usuarios[i].usuario, usuarios[i].senha);
+        printf("(%d) Aluno: %s (RGM: %d)\n", i, aluno[i].nome, aluno[i].RGM);
+        printf("    Situação: %s com média %.2f.\n\n", aluno[i].situacao, aluno[i].media);
     }
-    printf("\n");
 }
 
 // Opção 3 do menu
-void atualizarUsuarios(struct Login usuarios[], int total) {
+void atualizarAluno(struct Aluno aluno[], int total) {
     int indice;
 
-    printf("\nDigite o índice do usuário a ser atualizado: ");
+    printf("\n> Digite o índice do aluno a ser atualizado: ");
     scanf("%d", &indice);
 
     if (indice >= 0 && indice < total) {
-        printf("\nNovo nome de usuário (máx. 9 letras): ");
-        scanf("%9s", usuarios[indice].usuario);
+        printf("\n  Digite o novo nome completo (sem espaços): ");
+        scanf("%9s", aluno[indice].nome);
         
-        printf("Nova senha: ");
-        scanf("%d", &usuarios[indice].senha);
+        printf("  Digite o novo RGM: ");
+        scanf("%d", &aluno[indice].RGM);
 
-        printf("\nUsuário atualizado com sucesso!\n\n");
+        printf("  (*) Aluno atualizado com sucesso!\n\n");
     } else {
-        printf("- Índice inválido ou não existente.\n\n");
+        printf("  (!) Índice inválido ou não existente.\n\n");
     }
 }
 
 // Opção 4 do menu
-void deletarUsuarios(struct Login usuarios[], int *total) {
+void deletarAluno(struct Aluno aluno[], int *total) {
     int indice;
 
-    printf("\nDigite o índice do usuário a ser deletado: ");
+    printf("\n> Digite o índice do aluno a ser deletado: ");
     scanf("%d", &indice);
 
     if (indice >= 0 && indice < *total) {
         for (int i = indice; i < *total - 1; i++) {
-            usuarios[i] = usuarios[i + 1];
+            aluno[i] = aluno[i + 1];
         }
 
         (*total)--;
 
-        printf("\nUsuário deletado com sucesso!\n\n");
+        printf("  (*) Aluno deletado com sucesso!\n\n");
     } else {
-        printf("- Índice inválido ou não existente.\n\n");
+        printf("  (!) Índice inválido ou não existente.\n\n");
     }
+}
+
+// Opção 5 do menu
+void atribuirNotas(struct Aluno aluno[], int total) {
+    int indice;
+    float soma, novaSoma;
+
+    printf("\n> Digite o índice do aluno que receberá a média: ");
+    scanf("%d", &indice);
+
+    if (indice >= 0 && indice < total) {
+        // Lê a nota da A1
+        do {
+            printf("  Digite a nota da A1 (0-5): ");
+            scanf("%f", &aluno[indice].a1);
+        } while (aluno[indice].a1 < 0 || aluno[indice].a1 > 5);
+
+        // Lê a nota da A2
+        do {
+            printf("  Digite a nota da A2 (0-5): ");
+            scanf("%f", &aluno[indice].a2);
+        } while (aluno[indice].a2 < 0 || aluno[indice].a2 > 5);
+
+        // Calcula a média
+        soma = aluno[indice].a1 + aluno[indice].a2;
+
+        // Se a média for 6 ou mais, é aprovado
+        if (soma >= 6) {
+            printf("\n(*) %s foi aprovado com média %.2f.\n\n", aluno[indice].nome, soma);
+            aluno[indice].media = soma;
+            strcpy(aluno[indice].situacao, "Aprovado"); // Atribui "Aprovado" à situação do aluno
+        } else { // Senão, pede a AF
+            printf("\n(*) %s foi reprovado com média %.2f e deverá fazer AF.\n", aluno[indice].nome, soma);
+
+            // Lê a nota da AF
+            do {
+                printf("> Digite a nota da AF (0-5): ");
+                scanf("%f", &aluno[indice].af);
+            } while (aluno[indice].af < 0 || aluno[indice].af > 5);
+
+            // Substitui a menor nota entre A1 e A2 pela AF
+            if (aluno[indice].a1 < aluno[indice].a2) {
+                aluno[indice].a1 = aluno[indice].af;
+            } else {
+                aluno[indice].a2 = aluno[indice].af;
+            }
+
+            // Calcula a nova média
+            novaSoma = aluno[indice].a1 + aluno[indice].a2;
+
+            // Se a média for 6 ou mais, é aprovado
+            if (novaSoma >= 6) {
+                printf("\n(*) %s foi aprovado na AF com média %.2f.\n\n", aluno[indice].nome, novaSoma);
+                aluno[indice].media = novaSoma;
+                strcpy(aluno[indice].situacao, "Aprovado"); // Atribui "Aprovado" à situação do aluno
+            } else { // Senão, é reprovado
+                printf("\n(*) %s foi reprovado com média %.2f mesmo após a AF.\n\n", aluno[indice].nome, novaSoma);
+                aluno[indice].media = novaSoma;
+                strcpy(aluno[indice].situacao, "Reprovado"); // Atribui "Reprovado" à situação do aluno
+            }
+        }
+    } else {
+        printf("  (!) Índice inválido ou não existente.\n\n");
+    }
+}
+
+// Opção 6 do menu
+void atualizarNotas(struct Aluno aluno[], int total) {
+    int indice;
+    
+    printf("\n> Digite o índice do aluno que terá as notas atualizadas: ");
+    scanf("%d", &indice);
+
+    // TODO
+}
+
+// Opção 7 do menu
+void deletarNotas(struct Aluno aluno[], int total) {
+    int indice;
+
+    printf("\n> Digite o índice do aluno que terá suas notas deletadas: ");
+    scanf("%d", &indice);
+
+    // TODO
 }
 
 int main() {
     // Permite as acentuações em português
     setlocale(LC_ALL, "Portuguese");
 
-    struct Login usuarios[100];
-    int totalUsuarios = 0;
+    struct Aluno aluno[100];
+    int totalAlunos = 0;
     int escolha;
 
     do {
@@ -113,16 +195,25 @@ int main() {
 
         switch (escolha) {
             case 1:
-                criarUsuarios(usuarios, &totalUsuarios);
+                cadastrarAluno(aluno, &totalAlunos);
                 break;
             case 2:
-                listarUsuarios(usuarios, totalUsuarios);
+                listarAlunos(aluno, totalAlunos);
                 break;
             case 3:
-                atualizarUsuarios(usuarios, totalUsuarios);
+                atualizarAluno(aluno, totalAlunos);
                 break;
             case 4:
-                deletarUsuarios(usuarios, &totalUsuarios);
+                deletarAluno(aluno, &totalAlunos);
+                break;
+            case 5:
+                atribuirNotas(aluno, totalAlunos);
+                break;
+            case 6:
+                printf("Em andamento...\n\n");
+                break;
+            case 7:
+                printf("Em andamento...\n\n");
                 break;
             case 0:
                 // Opção 0 do menu
